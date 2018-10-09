b497bd4485df8680621373b1cf562a60d5908070
preethamntp@gmail.com <preethamntp@gmail.com>
2018-10-09 21:57:26 +0530

    completed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,11 @@ import { CurencyApiProvider } from '../../providers/curency-api/curency-api';
 export class HomePage {
   private converterForm: FormGroup;
   value: any;
+  currencies: any;
+  items: any;
+  countries: any;
   constructor(public navCtrl: NavController, private formBuilder: FormBuilder, public currencyService: CurencyApiProvider) {
+    this.getCountryList();
     this.converterForm = this.formBuilder.group({
       currencyValue: ['', Validators.required],
       currencyFrom: ['', Validators.required],
@@ -23,13 +27,53 @@ export class HomePage {
       currencyTo: this.converterForm.value.currencyTo,
       currencyValue: this.converterForm.value.currencyValue
     }
-
-    this.currencyService.getCurrencyRate().subscribe(data => {
-      localStorage.setItem('currencies', JSON.stringify(data));
+    if (!(JSON.parse(localStorage.getItem('currencies')))) {
+      this.currencyService.getCurrencyRate().subscribe(data => {
+        localStorage.setItem('currencies', JSON.stringify(data));
+        var currencies = JSON.parse(localStorage.getItem('currencies'));
+        reqObj.currencyValue = ((reqObj.currencyValue * currencies.rates[reqObj.currencyTo]) / currencies.rates[reqObj.currencyFrom])
+        console.log(reqObj.currencyValue);
+        this.value = reqObj.currencyValue;
+      });
+    }
+    else if (JSON.parse(localStorage.getItem('currencies'))) {
       var currencies = JSON.parse(localStorage.getItem('currencies'));
-      reqObj.currencyValue = ((reqObj.currencyValue * currencies.rates[reqObj.currencyFrom]) / currencies.rates[reqObj.currencyTo])
+      reqObj.currencyValue = ((reqObj.currencyValue * currencies.rates[reqObj.currencyTo]) / currencies.rates[reqObj.currencyFrom])
       console.log(reqObj.currencyValue);
       this.value = reqObj.currencyValue;
+    }
+  }
+  doRefresh(event) {
+    console.log('Begin async operation', event);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      localStorage.removeItem('currencies')
+      event.complete();
+    }, 2000);
+  }
+  getCountryList() {
+    this.currencyService.getCuntriesList().subscribe(response => {
+      this.items = response;
     });
   }
+
+  // getItems(ev: any) {
+  //   this.currencyService.getCuntriesList().subscribe(response => {
+  //     this.countries = response;
+
+  //     let val = ev.target.value;
+
+  //     if (val && val.trim() != '') {
+  //       this.countries = this.countries.filter((country) => {
+  //         return (country.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+  //       })
+  //     }
+  //   });
+  // }
+
+  // itemSelected(item) {
+  //   this.items = item;
+  //   this.converterForm
+  // }
 }
